46474bda4b91617c397a37bc90aa061a2c67ad1c
Matheus Santos <matheus_sa_santos@hotmail.com>
2020-01-01 13:04:40 -0300

    handle prev callback done

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -102,12 +102,9 @@ export default class Repository extends React.Component {
   handlePrev = () => {
     const { page, filterIndex } = this.state;
     if (page !== 1) {
-      this.setState(
-        {
-          page: page - 1,
-        },
-        () => this.handleFilter(filterIndex)
-      );
+      this.setState({
+        page: page - 1,
+      });
     }
   };
 
